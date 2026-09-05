1e0ea097473cb9336c5b50777c4560fda3de9329
imansardar <iman.sardar13898@gmail.com>
2026-09-05 13:09:35 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
@@ -6,7 +7,24 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    # منوی اصلی خودت اینجا
-    pass
+    markup = types.ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        row_width=2
+    )
+
+    markup.add(
+        types.KeyboardButton("🛒 خرید محصول"),
+        types.KeyboardButton("💰 افزایش موجودی"),
+        types.KeyboardButton("📦 محصولات من"),
+        types.KeyboardButton("💳 موجودی حساب"),
+        types.KeyboardButton("📞 پشتیبانی"),
+        types.KeyboardButton("🔙 برگشت")
+    )
+
+    bot.send_message(
+        message.chat.id,
+        "سلام رفیق 🌹\nبه ربات سردار خوش آمدید.\n\nلطفاً یکی از گزینه‌های زیر را انتخاب کنید:",
+        reply_markup=markup
+    )
 
 bot.infinity_polling()
